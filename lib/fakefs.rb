91de901babbdfead97234169e17edae502f525e4
Matt Freels <matt@freels.name>
2009-07-27 10:44:31 -0700

    make fakefs/safe pull in dependencies

--- lib/fakefs.rb
+++ lib/fakefs.rb
@@ -1,12 +1,3 @@
-require 'fileutils'
-require 'pathname'
-require 'fakefs/base'
-require 'fakefs/fake/file'
-require 'fakefs/fake/dir'
-require 'fakefs/fake/symlink'
-require 'fakefs/file_system'
-require 'fakefs/fileutils'
-require 'fakefs/file'
-require 'fakefs/dir'
+require 'fakefs/safe'
 
 FakeFS.activate!
--- lib/fakefs/safe.rb
+++ lib/fakefs/safe.rb
@@ -1,4 +1,13 @@
+require 'fileutils'
+require 'pathname'
 require 'fakefs/base'
+require 'fakefs/fake/file'
+require 'fakefs/fake/dir'
+require 'fakefs/fake/symlink'
+require 'fakefs/file_system'
+require 'fakefs/fileutils'
+require 'fakefs/file'
+require 'fakefs/dir'
 
 def FakeFS
   return ::FakeFS unless block_given?
