bdd31f88651eb3d0fcf9d56c3131c1807c67c075
David Miani <davidmiani@gmail.com>
2009-12-12 18:39:34 +1000

    improved FakeFsTestHelper to have methods for converting relative paths to a path in the temp directory being monitered

--- test/failing_fs_tests.rb
+++ test/failing_fs_tests.rb
@@ -4,24 +4,24 @@ class AutoTests < Test::Unit::TestCase
   include FakeFsTestHelper
 
   def test_symlink_creation
-    compare_with_real do |path|
+    compare_with_real do
       check_filesystem
       # Dir.mkdir(path + "somedir1" + rand().to_s)
-      Dir.mkdir(path + "somedir")
-      Dir.mkdir(path + "somedir/lol")
-      File.symlink(path + "somedir", path + "symlink")
+      Dir.mkdir mp("somedir")
+      Dir.mkdir mp("somedir/lol")
+      File.symlink(mp("somedir"), mp("symlink"))
       check_filesystem
     end
   end
 
 
   def test_dir_entries
-    compare_with_real do |path|
-      Dir.mkdir(path + "somedir")
-      Dir.mkdir(path + "another")
-      Dir.mkdir(path + "third")
+    compare_with_real do
+      Dir.mkdir mp("somedir")
+      Dir.mkdir mp("another")
+      Dir.mkdir mp("third")
       check_filesystem
-      check_value Dir.entries path
+      check_value Dir.entries (mp ("."))
     end
   end
       
