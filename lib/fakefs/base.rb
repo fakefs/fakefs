dc0d08d7c37e7758db9481b5129199777b7e0493
Lukas Oberhuber <lukaso@gmail.com>
2011-12-21 16:55:19 +0000

    Substitute Pathname to get tests to pass in ruby 1.9.3
    Moved pathname.rb from ruby 1.9.2 into project in order to continue compatibility to 1.9.3 where
    the functionality has been moved into c. In the c version of the file, the unset_const trick
    doesn't work because the constant rb_cFile in the c file is never modified. I think this is
    actually a bug in how ruby MRI operates, but the fix is a little bit complicated.

--- lib/fakefs/base.rb
+++ lib/fakefs/base.rb
@@ -2,6 +2,7 @@ RealFile            = File
 RealFileTest        = FileTest
 RealFileUtils       = FileUtils
 RealDir             = Dir
+RealPathname        = Pathname
 
 module FakeFS
   def self.activate!
@@ -10,11 +11,14 @@ module FakeFS
       remove_const(:File)
       remove_const(:FileTest)
       remove_const(:FileUtils)
+      remove_const(:Pathname) if RUBY_VERSION >= "1.9.3"
+      
 
       const_set(:Dir,       FakeFS::Dir)
       const_set(:File,      FakeFS::File)
       const_set(:FileUtils, FakeFS::FileUtils)
       const_set(:FileTest,  FakeFS::FileTest)
+      const_set(:Pathname,  FakeFS::Pathname) if RUBY_VERSION >= "1.9.3"
     end
     true
   end
@@ -25,11 +29,13 @@ module FakeFS
       remove_const(:File)
       remove_const(:FileTest)
       remove_const(:FileUtils)
+      remove_const(:Pathname) if RUBY_VERSION >= "1.9.3"
 
       const_set(:Dir,       RealDir)
       const_set(:File,      RealFile)
       const_set(:FileTest,  RealFileTest)
       const_set(:FileUtils, RealFileUtils)
+      const_set(:Pathname,  RealPathname) if RUBY_VERSION >= "1.9.3"
     end
     true
   end
