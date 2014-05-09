780fc6f8e6c82a93c25b9fb9e6630960e2cdd6b4
Carlos Pardo <cpardo@altavistaed.com>
2014-05-09 09:29:02 -0400

    Update absolute_path to use the fake file system's working directory.

--- lib/fakefs/file.rb
+++ lib/fakefs/file.rb
@@ -485,8 +485,8 @@ module FakeFS
     end
 
     if RUBY_VERSION >= "1.9.1"
-      def self.absolute_path(*args)
-        RealFile.absolute_path(*args)
+      def absolute_path(file_name, dir_name = Dir.getwd)
+        RealFile.absolute_path(file_name, dir_name)
       end
     end
 
--- test/fakefs_test.rb
+++ test/fakefs_test.rb
@@ -2560,6 +2560,11 @@ class FakeFSTest < Test::Unit::TestCase
       assert_equal '/foo/bar', File.absolute_path('/foo/bar', '/dir')
     end
 
+    def test_absolute_path_with_relative_path
+      FileUtils.touch('foo')
+      assert_equal "#{Dir.getwd}/foo/bar", File.absolute_path('foo/bar')
+    end
+
     def test_absolute_path_with_relative_path_with_dir_name
       FileUtils.touch('foo')
       assert_equal "/dir/foo/bar", File.absolute_path('foo/bar', '/dir')
