2c690e7b47c1933a226c1777ea01c8ad03370b0a
Brian Donovan <me@brian-donovan.com>
2015-04-23 11:57:13 -0700

    Fix rubocop offenses.

--- lib/fakefs/globber.rb
+++ lib/fakefs/globber.rb
@@ -58,14 +58,14 @@ module FakeFS
 
     def regexp(pattern)
       regex_body = pattern.gsub('.', '\.')
-                          .gsub('?', '.')
-                          .gsub('*', '.*')
-                          .gsub('(', '\(')
-                          .gsub(')', '\)')
-                          .gsub(/\{(.*?)\}/) do
-                            "(#{Regexp.last_match[1].gsub(',', '|')})"
-                          end
-                          .gsub(/\A\./, '(?!\.).')
+                   .gsub('?', '.')
+                   .gsub('*', '.*')
+                   .gsub('(', '\(')
+                   .gsub(')', '\)')
+                   .gsub(/\{(.*?)\}/) do
+                     "(#{Regexp.last_match[1].gsub(',', '|')})"
+                   end
+                   .gsub(/\A\./, '(?!\.).')
       /\A#{regex_body}\Z/
     end
 
