d2a0aa417596e90091d98da5a1f34f82db5bb538
Andrew Mackenzie <andrew@mackenzie-serres.net>
2016-03-20 14:41:13 +0100

    Added more detail on the failure

--- spec/fakefs/failing_spec.rb
+++ spec/fakefs/failing_spec.rb
@@ -24,6 +24,16 @@ describe 'failing test', fakefs: true do
   # When run with rspec alone it outputs
   # ArgumentError:
   #    could not find a temporary directory
+  #
+  # If you remove the before(:each) above, then it fails with this message:
+  # Errno::ENOENT: No such file or directory - /tmp/stderr20160320--9vv9lq
+  #                                                                      ./lib/fakefs/file.rb:649:in `create_missing_file'
+  # ./lib/fakefs/file.rb:405:in `initialize'
+  # ./lib/fakefs/dir.rb:188:in `create'
+  #                                                                                                                                              ./spec/fakefs/failing_spec.rb:25:in `block (2 levels) in <top (required)>'
+  # -e:1:in `load'
+  # -e:1:in `<main>'
+
   it ' should be able to expect output to stderr' do
     expect{ $stderr.puts 'Output to stderr' }.to output(/to stderr/).to_stderr_from_any_process
   end
