6272a43797796bf966f20e7bb2a0da4d97844b51
Benjamin Quorning <benjamin@quorning.net>
2016-10-05 20:17:19 +0200

    More output from failing test

--- test/fakefs_test.rb
+++ test/fakefs_test.rb
@@ -1265,8 +1265,8 @@ class FakeFSTest < Minitest::Test
     :pathconf
   ]
 
-  def test_every_method_in_file_is_in_fake_fs_file
-    (RealFile.instance_methods - OMITTED_FILE_METHODS).each do |method_name|
+  (RealFile.instance_methods - OMITTED_FILE_METHODS).each do |method_name|
+    define_method("test_#{method_name}_method_in_file_is_in_fake_fs_file") do
       assert File.instance_methods.include?(method_name), "#{method_name} method is not available in File :("
     end
   end
