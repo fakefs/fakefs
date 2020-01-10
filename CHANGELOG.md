8b8f1fac46879bcad503191f56ea6fe76c2299ae
Michael Grosser <michael@grosser.it>
2020-01-10 12:13:03 -0800

    v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## Next
+
+## 1.0.0
+
+- No changes, this is a cosmetic release to signal stableness
--- lib/fakefs/version.rb
+++ lib/fakefs/version.rb
@@ -1,7 +1,7 @@
 module FakeFS
   # Version module
   module Version
-    VERSION = '0.20.1'.freeze
+    VERSION = '1.0.0'.freeze
 
     def self.to_s
       VERSION
