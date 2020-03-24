fakefs/fakefs

commit 444c4b6c5e453de56bce2dce5ea2c6061bf8c430

FakeFS::Dir: fix usage of options in ruby 2.7

lib/fakefs/dir.rb
@@ -194,16 +194,15 @@ module FakeFS
         if (opts = Hash.try_convert(rest[-1]))
           opts = opts.dup if rest.pop.equal?(opts)
           max_try = opts.delete(:max_try)
-          opts = [opts]
         else
-          opts = []
+          opts = {}
         end
         tmpdir, = *rest
         tmpdir ||= self.tmpdir # rubocop:disable Style/RedundantSelf
         n = nil
         begin
           path = File.join(tmpdir, make_tmpname(basename, n))
-          yield(path, n, *opts)
+          yield(path, n, **opts)
         rescue Errno::EEXIST
           n ||= 0
           n += 1

commit 35f943e521915653d058be305118999693c126c7

FakeDir::Dir::Tmpname: drop unecessary ** operator
This was a mistake introduced by me in
444c4b6c5e453de56bce2dce5ea2c6061bf8c430. The block on the other side of
that call (in 'tempfile.rb' from the standard library) does not expect
keyword arguments, but the actual Hash.

lib/fakefs/dir.rb
@@ -202,7 +202,7 @@ module FakeFS
         n = nil
         begin
           path = File.join(tmpdir, make_tmpname(basename, n))
-          yield(path, n, **opts)
+          yield(path, n, opts)
         rescue Errno::EEXIST
           n ||= 0
           n += 1