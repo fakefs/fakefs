5596b63655816e0745c451c3a7b030c6c6a13e11
Michael Grosser <michael@grosser.it>
2022-06-19 08:35:47 -0700

    remove global methods added by tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,6 @@ require 'bundler/setup'
 require 'fakefs/safe'
 require 'maxitest/autorun'
 
-def act_on_real_fs(&block)
-  FakeFS.without(&block)
-end
-
 module Minitest
   class Test
     # Minitest::Test#diff needs to write to the filesystem in order to produce
@@ -14,30 +10,34 @@ module Minitest
     def diff(expected, actual)
       act_on_real_fs { super(expected, actual) }
     end
+
+    def act_on_real_fs(&block)
+      FakeFS.without(&block)
+    end
+
+    def capture_stderr
+      real_stderr, $stderr = $stderr, StringIO.new
+
+      # force FileUtils to use our stderr
+      RealFileUtils.instance_variable_set('@fileutils_output', $stderr)
+
+      yield
+
+      $stderr.string
+    ensure
+      $stderr = real_stderr
+
+      # restore FileUtils stderr
+      RealFileUtils.instance_variable_set('@fileutils_output', $stderr)
+    end
+
+    def real_file_sandbox(path = nil)
+      base_path = real_file_sandbox_path
+      path ? File.join(base_path, path) : base_path
+    end
+
+    def real_file_sandbox_path
+      File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_sandbox'))
+    end
   end
-end
-
-def capture_stderr
-  real_stderr, $stderr = $stderr, StringIO.new
-
-  # force FileUtils to use our stderr
-  RealFileUtils.instance_variable_set('@fileutils_output', $stderr)
-
-  yield
-
-  $stderr.string
-ensure
-  $stderr = real_stderr
-
-  # restore FileUtils stderr
-  RealFileUtils.instance_variable_set('@fileutils_output', $stderr)
-end
-
-def real_file_sandbox(path = nil)
-  base_path = real_file_sandbox_path
-  path ? File.join(base_path, path) : base_path
-end
-
-def real_file_sandbox_path
-  File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_sandbox'))
 end
